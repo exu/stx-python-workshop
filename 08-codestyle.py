c455c3c7a655c31e3f84b18d6c3cfae65f859c7b
Jacek Wysocki <jacek.wysocki@gmail.com>
2014-11-15 19:53:02 +0100

    zadanko 8

---
+++ 08-codestyle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import random
+
+class Aaaaaaaaa:
+    lalalal = 23
+    kokokoko = 888
+
+    def ajaajaj():
+        pass
+    def bbbb():
+        pass
