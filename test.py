472f548aa0e1dc0d1b7b609037331e02984ee0b5
Jacek Wysocki <jacek.wysocki@gmail.com>
2014-11-15 19:53:14 +0100

    example test

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class BlaBlaTest(unittest.TestCase):
+    def test_vlablablabla(self):
+        self.assertEquals()
+
+
+if __name__ == '__main__':
+    unittest.main()
